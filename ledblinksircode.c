5200a45ee1c420a92d7bf30fe7a95d406d8b1eb8
Moneeshwaran <65331701+Moneeshwaran@users.noreply.github.com>
2022-12-20 13:07:49 +0530

    Update ledblinksircode.c

--- ledblinksircode.c
+++ ledblinksircode.c
@@ -1,77 +1,49 @@
-#define uint32 unsigned int
+#define unit32 unsigned int
 	
-volatile uint32 *RCC_CR=0x40023800;
-volatile uint32 *RCC_AHB1ENR=0x40023800+0x30;
-
-volatile uint32 *GPIOG_MODER=0x40021800;
-volatile uint32 *GPIOG_OTYPER=0x40021800+0x04;
-volatile uint32 *GPIOG_OSPEEDR=0x40021800+0x08;
-volatile uint32 *GPIOG_PUPDR=0x40021800+0x0c;
-volatile uint32 *GPIOG_ODR=0x40021800+0x14;
+volatile unit32 *RCC_CR = 0x40023800;
+volatile unit32 *RCC_AHB1ENR = 0x40023800 + 0x30;
+volatile unit32 *GPIOG_MODER = 0x40021800;
+volatile unit32 *GPIOG_OTYPER = 0x40021800 + 0x04;
+volatile unit32 *GPIOG_OSPEEDR = 0x40021800 + 0x08;
+volatile unit32 *GPIOG_PUPDR = 0x40021800 + 0x0C;
+volatile unit32 *GPIOG_ODR = 0x40021800 + 0x14;
 
 void clock_internal_enable();
 void gpio_init();
 void blink_led();
 
-void main()
+int main()
 {
 	clock_internal_enable();
 	gpio_init();
-	
-	/*while(1)
+	while (1)
 	{
-	if(sw==0)
-	{
-	*GPIOG_ODR^=(1<<13);
-	}
-	
-	}*/
-	
-	
-	
-	while(1){
 		blink_led();
 	}
 }
-
 void clock_internal_enable()
 {
-	*RCC_CR|=(1<<0);//enabling the internal clock
-	*RCC_AHB1ENR|=(1<<6);//enabling the internal GPIOG
+	*RCC_CR |=(1<<0);
+	*RCC_AHB1ENR = (1<<6);
 }
-
 void gpio_init()
 {
-	*GPIOG_MODER |= (1<<26);//enabling pin13 as o/p
-	*GPIOG_MODER &= ~(1<<27);//enabling pin13 as o/p
-	*GPIOG_OTYPER &=~(1<<13);//making pin13 o/p type as push pull
-	*GPIOG_OSPEEDR &=~(1<<27);//enabling medium speed for pin 13
-	*GPIOG_OSPEEDR |=(1<<26);//enabling medium speed for pin 13
-	
-	*GPIOG_PUPDR&=~(1<<27);//enabling no pull push for pin13
-	*GPIOG_PUPDR&=~(1<<26);//enabling no pull push for pin13
+	*GPIOG_MODER |= (1<<26);
+	*GPIOG_MODER &= ~(1<<27);
+	*GPIOG_OTYPER &= ~(1<<13);
+	*GPIOG_OSPEEDR &= ~(1<<27);
+	*GPIOG_OSPEEDR |= (1<<26);
+	*GPIOG_PUPDR &= ~(1<<27);
+	*GPIOG_PUPDR &= ~(1<<26);
 }
-	/*for switch
-	*GPIOG_MODER |= (0<<swpinno);//enabling pin13 as ip
-	*GPIOG_MODER &= ~(0<<27);//enabling pin13 as i/p
-	*GPIOG_OTYPER &=~(1<<13);//making pin13 o/p type as push pull
-	*GPIOG_OSPEEDR &=~(1<<27);//enabling medium speed for pin 13
-	*GPIOG_OSPEEDR |=(1<<26);//enabling medium speed for pin 13
-	
-	*GPIOG_PUPDR&=~(1<<27);//enabling no pull push for pin13
-	*GPIOG_PUPDR&=~(1<<26);//enabling no pull push for pin13
-	
-	
-}*/
-
-void blink_led(){
-	for(uint32 i=0;i<100000;i++)
+void blink_led()
+{
+	for (unit32 i=0; i<900000; i++)
 	{
-		*GPIOG_ODR|=(1<<13);
+		*GPIOG_ODR |=(1<<13);
 	}
-	for(uint32 i=0;i<100000;i++)
+	for (unit32 i=0; i<100000; i++)
 	{
-		*GPIOG_ODR&=~(1<<13);
+		*GPIOG_ODR &= ~(1<<13);
 	}
-}                                                                                              
-	
+}
